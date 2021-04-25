4c956331b133615b90ed06d0acdfbc3a5f526a13
souza-anderson <andersonfebs@gmail.com>
2021-04-24 21:33:25 -0400

    add env port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const indexRouter = require('./routes/index')
 const tabuadaRouter = require('./routes/tabuada')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
