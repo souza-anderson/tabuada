f7e7182e4ee134f75f845f6c3bab967970dac7d0
Anderson Souza <andersonfebs@gmail.com>
2021-04-26 12:15:00 -0400

    change path index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/tabuadas', tabuadaRouter)
